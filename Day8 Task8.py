e8344e2e0f1528f571fc25463159a8d47daee4fb
LEENACHELSIN <leenachelsin@gmail.com>
2025-10-15 17:06:28 +0530

    Add files via upload

---
+++ Day8 Task8.py
@@ -0,0 +1,26 @@
+#Task1
+list=[]
+sum=0
+num=int(input("Enter your number:"))
+while num>0:
+    sum+=num
+    num=int(input("Enter your number:"))
+print(sum)
+#Task2
+list=[2,-4,5,-8,7,9,-1]
+i=0
+p1=[]
+n1=[]
+for i in list:
+    if i>0:
+        p1.append(i)
+    else:
+        p1.append(i)
+print (p1)
+print (n1)
+
+
+
+    
+
+      
